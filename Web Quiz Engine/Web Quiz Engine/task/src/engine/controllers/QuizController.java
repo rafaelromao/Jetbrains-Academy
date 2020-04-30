a9fb7cd52b0d950d6094bf667b1905ac3f6751a0
rafaelromao <rafaelromao@gmail.com>
2020-04-30 16:49:33 -0300

    stage 2 of Web Quiz Engine project

--- Web Quiz Engine/Web Quiz Engine/task/src/engine/controllers/QuizController.java
+++ Web Quiz Engine/Web Quiz Engine/task/src/engine/controllers/QuizController.java
@@ -2,23 +2,73 @@ package engine.controllers;
 
 import engine.models.Quiz;
 import engine.models.Feedback;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.stream.Collectors.toList;
+
 @RestController
-@RequestMapping("api/quiz")
+@RequestMapping("api/quizzes")
 public class QuizController {
 
+    private List<Quiz> quizzes = new ArrayList<>();
+
     @GetMapping
-    public Quiz get() {
-        return new Quiz("title", "text", new String[]{"1", "2", "correct", "4"});
+    public ResponseEntity<List<Quiz>> get() {
+        return ResponseEntity.ok(
+                quizzes.stream().collect(toList()));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Quiz> get(@PathVariable String id) {
+        var quiz = getQuiz(id);
+        if (quiz.isPresent()) {
+            return ResponseEntity.ok(
+                    quizzes.stream().filter(q -> q.getId() == Integer.parseInt(id)).findAny().get());
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable String id) {
+        var quiz = getQuiz(id);
+        if (quiz.isPresent()) {
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
-    public Feedback post(@RequestParam String answer) {
-        if ("2".equals(answer)) {
-            return new Feedback(true, "Congratulations, you're right!");
+    public ResponseEntity<Quiz> post(@RequestBody Quiz quiz) {
+        quiz.setId(quizzes.size());
+        quizzes.add(quiz);
+        return ResponseEntity.ok(quiz);
+    }
+
+    @PostMapping("{id}/solve")
+    public ResponseEntity<Feedback> post(@PathVariable String id, @RequestParam String answer) {
+        var quiz = getQuiz(id);
+        if (quiz.isPresent()) {
+            if (quiz.get().getAnswer() == Integer.parseInt(answer)) {
+                return ResponseEntity.ok(
+                        new Feedback(true, "Congratulations, you're right!"));
+            }
+            return ResponseEntity.ok(
+                    new Feedback(false, "Wrong answer! Please try again.")
+            );
         }
-        return new Feedback(false, "Wrong answer! Please try again.");
+        return new ResponseEntity(HttpStatus.NOT_FOUND);
+    }
+
+    private Optional<Quiz> getQuiz(String id) {
+        var answerValue = Integer.parseInt(id);
+        var quiz = quizzes.stream().filter(q -> q.getId() == answerValue).findAny();
+        return quiz;
     }
 
 }
--- Web Quiz Engine/Web Quiz Engine/task/src/engine/models/Quiz.java
+++ Web Quiz Engine/Web Quiz Engine/task/src/engine/models/Quiz.java
@@ -1,12 +1,16 @@
 package engine.models;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Quiz {
+    private int id;
     private String title;
     private String text;
     private String[] options;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private int answer;
 }
