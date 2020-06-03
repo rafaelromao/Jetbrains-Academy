67a1404dabdb5a87465eee2e67092aff0ef19e36
Rafael  Romao <rafaelromao@gmail.com>
2020-06-03 19:11:07 -0300

    add some problems

--- File Type Analyzer/Problems/The number of equal substring pairs/src/Main.java
+++ File Type Analyzer/Problems/The number of equal substring pairs/src/Main.java
@@ -17,51 +17,60 @@ public class Main {
                 ijkns[i][2] = scanner.nextInt();
                 ijkns[i][3] = scanner.nextInt();
             }
-            var prefixHashes = getPrefixHashes(s);
+            var prefixHashes = prefixHashes(s);
             var count = 0;
             for (var ijkn : ijkns) {
-                var sij = getSubstringHash(prefixHashes, ijkn, 0, 1);
-                var skn = getSubstringHash(prefixHashes, ijkn, 2, 3);
+                var sij = substringHash(prefixHashes, ijkn, 0, 1);
+                var skn = substringHash(prefixHashes, ijkn, 2, 3);
                 if (sij == skn) count++;
             }
             System.out.println(count);
         }
     }
 
-    private static long getSubstringHash(List<Long> prefixHashes, int[] ijkn, int ik, int jn) {
+    private static long substringHash(List<Long> prefixHashes, int[] ijkn, int ik, int jn) {
         var i = ijkn[ik];
         var j = ijkn[jn];
-        var pj = prefixHashes.get(j);
-        var pi = prefixHashes.get(i);
-        var ai = POWS.get(i);
-        return (pj - pi + M) % M / ai;
+        var pj = prefixHashes.get(j - 1);
+        var pi = i > 0 ? prefixHashes.get(i - 1) : 0;
+        var ai = i > 0 ? POWS.get(i - 1) : 1;
+        var p = (pj - pi + M) % M;
+        return p / ai;
     }
 
-    private static List<Long> getPrefixHashes(String text) {
+    private static List<Long> prefixHashes(String text) {
         var hashes = new ArrayList<Long>();
-        for (var limit = 0; limit <= text.length(); limit++) {
-            var hash = hash(text, limit);
+        var hash = 0L;
+        for (var index = 0; index < text.length(); index++) {
+            hash = hash(text, index, hash);
             hashes.add(hash);
         }
         return hashes;
     }
 
-    private static long hash(String text, int limit) {
-        var hash = 0L;
-        var pow = 1L;
-        for (var i = 0; i < limit; i++) {
-            var includedChar = text.charAt(i);
-            var includedLong = charToLong(includedChar);
-            hash += includedLong * pow;
-            hash %= M;
-            if (POWS.size() < i + 1) {
-                POWS.add(pow);
-                pow = pow * A % M;
-            } else {
-                pow = POWS.get(i);
-            }
-        }
+    private static long hash(String text, int index, long hash) {
+        var pow = pow(index);
+        var includedChar = text.charAt(index);
+        var includedLong = charToLong(includedChar);
+        hash += includedLong * pow;
+        hash %= M;
         return hash;
+    }
+
+    private static long pow(int index) {
+        var pow = 1L;
+        if (POWS.size() > 0) {
+            if (POWS.size() > index) {
+                pow = POWS.get(index);
+            } else {
+                pow = POWS.get(POWS.size() - 1);
+                pow = pow * A % M;
+                POWS.add(pow);
+            }
+        } else {
+            POWS.add(pow);
+        }
+        return pow;
     }
 
     public static long charToLong(char ch) {
