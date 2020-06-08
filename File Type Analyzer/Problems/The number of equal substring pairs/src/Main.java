fc39629d77047e7f33393b35ccb695b4248227eb
Rafael Romao <rafaelromao@gmail.com>
2020-06-08 18:10:06 -0300

    fix code for most cases

--- File Type Analyzer/Problems/The number of equal substring pairs/src/Main.java
+++ File Type Analyzer/Problems/The number of equal substring pairs/src/Main.java
@@ -1,41 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    private static final int A = 53;
+    private static final long A = 53;
     private static final long M = 1_000_000_000 + 9L;
     private static final List<Long> POWS = new ArrayList<>();
 
-    public static void main(String[] args) {
-        try (var scanner = new Scanner(System.in)) {
-            var s = scanner.nextLine().trim();
-            var t = scanner.nextInt();
+    public static void main(String[] args) throws IOException {
+        try (var reader = new InputStreamReader(System.in);
+             var buffer = new BufferedReader(reader)) {
+            var s = buffer.readLine();
+            var t = Integer.parseInt(buffer.readLine());
             var ijkns = new int[t][];
             for (var i = 0; i < t; i++) {
-                ijkns[i] = new int[4];
-                ijkns[i][0] = scanner.nextInt();
-                ijkns[i][1] = scanner.nextInt();
-                ijkns[i][2] = scanner.nextInt();
-                ijkns[i][3] = scanner.nextInt();
+                ijkns[i] = Arrays.stream(buffer.readLine().split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
             }
             var prefixHashes = prefixHashes(s);
             var count = 0;
             for (var ijkn : ijkns) {
-                var sij = substringHash(prefixHashes, ijkn, 0, 1);
-                var skn = substringHash(prefixHashes, ijkn, 2, 3);
-                if (sij == skn) count++;
+                var hsij = substringHashWithMultiplier(prefixHashes, ijkn, 0, 1);
+                var hskn = substringHashWithMultiplier(prefixHashes, ijkn, 2, 3);
+
+                //Compensate the multipliers (p^i factors), according to https://cp-algorithms.com/string/string-hashing.html
+                if (ijkn[0] < ijkn[2]) {
+                    hsij *= POWS.get(ijkn[2] - ijkn[0]);
+                } else {
+                    hsij *= POWS.get(ijkn[0] - ijkn[2]);
+                }
+                hsij %= M;
+                hskn %= M;
+
+                if (hsij == hskn) count++;
             }
             System.out.println(count);
         }
     }
 
-    private static long substringHash(List<Long> prefixHashes, int[] ijkn, int ik, int jn) {
+    private static long substringHashWithMultiplier(List<Long> prefixHashes, int[] ijkn, int ik, int jn) {
         var i = ijkn[ik];
         var j = ijkn[jn];
-        var pj = prefixHashes.get(j - 1);
-        var pi = i > 0 ? prefixHashes.get(i - 1) : 0;
-        var ai = POWS.get(i);
-        var p = (pj - pi + M) % M;
-        return p / ai;
+        if (i > j) {
+            throw new IllegalArgumentException();
+        }
+        var hpj = prefixHashes.get(j - 1);
+        var hpi = i == 0 ? 0 : prefixHashes.get(i - 1);
+        var hij = (hpj - hpi + M) % M;
+        return hij;
     }
 
     private static List<Long> prefixHashes(String text) {
--- File Type Analyzer/Problems/The number of equal substring pairs/src/MainWrapper.java
+++ File Type Analyzer/Problems/The number of equal substring pairs/src/MainWrapper.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class MainWrapper {
     public static void main(String[] args) throws IOException {
-        FileInputStream is = new FileInputStream(new File("/Users/rromao/Downloads/hyperskill-9636-test-01.txt"));
+        FileInputStream is = new FileInputStream(new File("/Users/rromao/Downloads/hyperskill-5723-test-06.txt"));
         System.setIn(is);
         Main.main(args);
     }
