8bf7f5b935d591b01ea1b4ba7d97893f18808ff0
Rafael Romao <rafaelromao@gmail.com>
2020-06-10 19:56:55 -0300

    convert nested XML elements and attribbutes - refactored

--- JSON - XML converter/JSON - XML converter/task/src/converter/Converter.java
+++ JSON - XML converter/JSON - XML converter/task/src/converter/Converter.java
@@ -9,4 +9,33 @@ interface Converter {
             return input.charAt(0) == '<' ? new XML2JSONConverter() : new JSON2XMLConverter();
         }
     }
+
+    class JSON2XMLConverter implements Converter {
+
+        @Override
+        public String convert(String content) {
+            XMLWriter writer = new XMLWriter();
+            var reader = new JSONReader();
+            var value = reader.readObject(content);
+            var properties = value.split("(?!\\B\\{[^\\}]*),(?![^\\{]*\\}\\B)");
+            if (properties.length == 1) {
+                var keyValuePair = reader.readProperty(properties[0]);
+                writer.writeElement(reader, keyValuePair[0], keyValuePair[1]);
+            }
+            return writer.toString();
+        }
+    }
+
+    class XML2JSONConverter implements Converter {
+        @Override
+        public String convert(String content) {
+            var reader = new XMLReader();
+            var element = reader.readElement(content);
+            var writer = new JSONWriter();
+            writer.writeBeginObject();
+            writer.writeProperty(element[0], element[1]);
+            writer.writeEndObject();
+            return writer.toString();
+        }
+    }
 }
--- JSON - XML converter/JSON - XML converter/task/src/converter/XMLWriter.java
+++ JSON - XML converter/JSON - XML converter/task/src/converter/XMLWriter.java
@@ -1,5 +1,7 @@
 package converter;
 
+import java.util.Arrays;
+
 class XMLWriter {
     private StringBuilder builder = new StringBuilder();
 
@@ -45,6 +47,52 @@ class XMLWriter {
 
     void writeValue(String value) {
         builder.append(value == null ? "" : value);
+    }
+
+    void writeElement(JSONReader reader, String name, String value, String... attributes) {
+        var elementType = JSONReader.ElementType.of(value);
+        switch (elementType) {
+            case LITERAL:
+            case STRING:
+                writeLiteral(name, value, attributes);
+                break;
+            case OBJECT:
+                writeObject(reader, name, value);
+        }
+    }
+
+    void writeObject(JSONReader reader, String name, String value) {
+        var properties = reader.readObject(value)
+                .split("(?!\\B\\{[^\\}]*),(?![^\\{]*\\}\\B)");
+        var attributes = Arrays.stream(properties)
+                .filter(p -> p.startsWith("\"@"))
+                .toArray(String[]::new);
+        var elements = Arrays.stream(properties)
+                .filter(p -> !p.startsWith("\"@"))
+                .toArray(String[]::new);
+        if (elements.length == 0) {
+            writeElement(name, attributes);
+        } else {
+            writeBeginElement(name, attributes);
+            for (var element : elements) {
+                var keyValuePair = reader.readProperty(element);
+                writeElement(
+                        reader,
+                        keyValuePair[0],
+                        keyValuePair[1]);
+            }
+        }
+        writeEndElement(name);
+    }
+
+    void writeLiteral(String name, String value, String... attributes) {
+        if (value == null || value.length() == 0) {
+            writeElement(name, attributes);
+        } else {
+            writeBeginElement(name, attributes);
+            writeValue(value);
+            writeEndElement(name);
+        }
     }
 
     @Override
